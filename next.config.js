a85dac80de1bdfc420982931ced211ec32f4e0e7
aurora <aurora.makovac@gmail.com>
2023-03-20 18:41:24 +0100

    changing img to imgage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.w.kodadot.xyz',
+        port: '',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
